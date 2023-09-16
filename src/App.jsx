d7f6ef90574eb640c32ee312687ba49eb73fdb16
jainvedant392 <jainvedant392@gmail.com>
2023-09-17 00:24:22 +0530

    feat: Setup Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import React from "react";
+import { Route, Routes } from 'react-router-dom';
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<LandingPage />} ></Route>
+        <Route path="/login" element={<LoginPage />} ></Route>
+        <Route path="/signup" element={<SignupPage />} ></Route>
+      </Routes>
+    </>
+  )
 }
 
 export default App;
