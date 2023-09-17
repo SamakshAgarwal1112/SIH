d1c7c9b975a6cfd7a7b3e6eff4103de0463bb41d
NightFury742 <eshaan122@gmail.com>
2023-09-17 12:08:48 +0530

    feat: landing page created

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Flex, Text, Box, Link } from '@chakra-ui/react';
+
+function Navbar() {
+  return (
+    <>
+      <Box w="100%" top="0">
+        <Box h="2rem" bg="#000000" w="100%"></Box>
+        <Flex
+          bg="#83A98E"
+          w="100%"
+          h="5rem"
+          align="center"
+          justify="space-between"
+          pl="1.5rem"
+          pr="4.5rem"
+        >
+          <Link to="/">
+            <Text
+              fontSize="2xl"
+              fontWeight="extrabold"
+              color="#ffffff"
+              ml="1rem"
+            >
+              SANGAM
+            </Text>
+          </Link>
+          <Flex
+            align="center"
+            justify="space-between"
+            color="#ffffff"
+            fontSize="1.2rem"
+            fontWeight="light"
+            w="21.8rem"
+          >
+            <Link>
+              <Text>Lorem</Text>
+            </Link>
+            <Link>
+              <Text>Lorem</Text>
+            </Link>
+            <Link>
+              <Text>Lorem</Text>
+            </Link>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
+}
+
+export default Navbar;
